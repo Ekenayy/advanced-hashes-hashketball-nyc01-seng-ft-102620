86b91b61d6fff62ce7046663cd1b354ff2ba9024
Ekene N <ekenayy@gmail.com>
2020-10-16 01:11:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -164,7 +164,6 @@ def team_names
     name_array = []
 
 hashketball.each do |key, value|
-  value.each do |team, names|
   if value.has_value?("Brooklyn Nets") || value.has_value?("Charlotte Hornets")
     name_array << names
  # binding.pry
