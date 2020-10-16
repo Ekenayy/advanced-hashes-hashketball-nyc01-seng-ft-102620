7f4ca0573429793c2082745e994c0bbf134ebbc7
Ekene N <ekenayy@gmail.com>
2020-10-16 01:43:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,7 @@ i = 0
 hashketball.each do |key, value|
     if value.has_value?(team)
       while i < 5 do
-     player_numbers << hashketball[team][:players][i][:number]
+     #player_numbers << hashketball[team][:players][i][:number]
      i += 1
     #binding.pry
       end
