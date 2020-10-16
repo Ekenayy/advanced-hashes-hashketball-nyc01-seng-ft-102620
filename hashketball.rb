140dcd248a74de68cdde66af9a36d17e69ceb5fd
Ekene N <ekenayy@gmail.com>
2020-10-16 02:06:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -209,7 +209,7 @@ def player_stats(given_player)
   
   hashketball.each do |key, value|
    value[:players].each do |stats, data|
-    if stats[:player_name].has_value?(player_name)
+    if stats[:player_name].has_value?(given_player)
      binding.pry
     end 
   end
